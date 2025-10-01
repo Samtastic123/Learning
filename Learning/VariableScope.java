9196db32bb91111dd135659a296afd8dd1a2c87b
Samtastic123 <reemasilya@gmail.com>
2025-10-01 00:24:39 -0700

    All my learning

--- Learning/VariableScope.java
+++ Learning/VariableScope.java
@@ -4,5 +4,9 @@ public class VariableScope {
     public static void main(String[] args){
 
         //variable scope : where variables can be accessed
+
+        int x = 1; //LOCAL SCOPE
+
+        System.out.println(x);
     }
 }
