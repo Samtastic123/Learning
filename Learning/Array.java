91e03dab39352aa98bc0f2e9b88c8479065f6e8e
Samtastic123 <reemasilya@gmail.com>
2025-10-03 23:56:26 -0700

    All my learning

---
+++ Learning/Array.java
@@ -0,0 +1,20 @@
+package Learning;
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        // Arrays are a collection of values of the same data type
+        //                 in other words, it is a variable that can store more than 1 value
+
+        String[] fruits = {"Banana", "Orange", "Apple", "Coconut"};
+
+        //fruits[0] = "pineapple";
+        //int numOfFruits = fruits.length;
+        //Arrays.sort(fruits);
+        //Arrays.fill(fruits, "pineapple");
+
+        for(String fruit : fruits){
+            System.out.println(fruit);
+        }
+    }
+}
